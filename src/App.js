d7cd8bb2e9485c5e76a08b10fe9e292cbc892ed7
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-11-10 18:52:03 -0300

    exercicio 1

--- src/App.js
+++ src/App.js
@@ -3,13 +3,38 @@ import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.nextPokemon = this.nextPokemon.bind(this);
+
+    this.state = {
+      index: 0,
+      currentPokemon: pokemons
+    }
+
+  }
+  nextPokemon() {
+    if (this.state.index === this.state.currentPokemon.length - 1) {
+      this.setState({
+        index: 0
+      })
+    } else {
+      this.setState((prevState, _props) => ({
+        index: prevState.index + 1,
+      }))
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1> Pokedex </h1>
+        <Pokedex pokemons={this.state.currentPokemon[this.state.index]} />
+        <button onClick={this.nextPokemon}>Proximo Pokemon</button>
+      </div>
+    );
+  }
 }
 
 export default App;
